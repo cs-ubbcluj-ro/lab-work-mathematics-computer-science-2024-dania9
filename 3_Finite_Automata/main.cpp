4311f0c804466daf0a66f99382007da5b3ff8195
dania9 <127238745+dania9@users.noreply.github.com>
2024-12-02 19:34:28 +0200

    Create main.cpp

---
+++ 3_Finite_Automata/main.cpp
@@ -0,0 +1,8 @@
+#include "FINITEAUTOMATA.h"
+
+int main() {
+    FiniteAutomata fa;
+    fa.loadFromFile("C:\\Users\\onofr\\source\\repos\\Lab3L\\Lab3L\\FA.in");
+    fa.displayMenu();
+    return 0;
+}
